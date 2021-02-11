8f76cfb38ff083dd0021727d7559bb931288a1f5
Harsh Lathwal <xeon.harsh@gmail.com>
2021-02-11 23:54:29 +0530

    Update full_tomato.mk

--- full_tomato.mk
+++ full_tomato.mk
@@ -16,7 +16,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Inherit from tomato device
 $(call inherit-product, device/yu/tomato/device.mk)
